b81459a1506bd7f6f76d8dafc6585cdf04ff5b6d
utkarsh singh <utkarsh.singh17025@gmail.com>
2026-01-17 23:43:48 -0800

    added custom collate_fn()

--- model/dataloader.py
+++ model/dataloader.py
@@ -1,8 +1,5 @@
 import torch
 from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
 import pandas as pd
 
 class ProteinLigandDataset(Dataset):
@@ -20,3 +17,27 @@ class ProteinLigandDataset(Dataset):
             "ligand_smiles":row["ligand_smiles"]
         }
 
+def collate_protein_ligand(batch,protein_tokenizer,ligand_tokenizer):
+    proteins = [b["protein"] for b in batch]
+    ligands = [b["ligand_smiles"] for b in batch]
+    ids = [b["complex_id"] for b in batch]
+
+    protein_tokens = protein_tokenizer(
+        proteins,
+        padding = True,
+        truncation = True,
+        return_tensors = "pt"
+    )
+
+    ligand_tokens = ligand_tokenizer(
+        ligands,
+        padding=True,
+        truncation = True,
+        return_tensors = "pt"
+    )
+
+    return {
+        "protein": protein_tokens,
+        "ligand" : ligand_tokens,
+        "id":ids
+    }
