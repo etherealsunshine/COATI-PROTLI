8f8b0c9cfb4d117ee012ac9a55174ddb5bc6930c
utkarsh singh <utkarsh.singh17025@gmail.com>
2026-01-19 11:30:48 -0800

    amended dataloader to provide embeddings

--- model/dataloader.py
+++ model/dataloader.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 
-from model.precompute_ligand_embeddings import ligand_embeddings
+#from model.precompute_ligand_embeddings import ligand_embeddings
 
 class ProteinLigandDataset(Dataset):
     def __init__(self,data_path,ligand_emb_path):
@@ -18,13 +18,13 @@ class ProteinLigandDataset(Dataset):
             "complex_id":row["complex_id"],
             "protein":row["protein"],
             #"ligand_smiles":row["ligand_smiles"]
-            "ligand_embdding":self.ligand_embs[row["complex_id"]]
+            "ligand_embedding":self.ligand_embs[row["complex_id"]]
         }
 
-def collate_protein_ligand(batch,protein_tokenizer,ligand_tokenizer):
+def collate_protein_ligand(batch,protein_tokenizer):
     proteins = [b["protein"] for b in batch]
     #ligands = [b["ligand_smiles"] for b in batch]
-    ligand_embeddings = torch.stack(b["ligand_embeddings"] for b in batch)
+    ligand_embeddings = torch.stack([b["ligand_embedding"] for b in batch])
     ids = [b["complex_id"] for b in batch]
 
     protein_tokens = protein_tokenizer(
